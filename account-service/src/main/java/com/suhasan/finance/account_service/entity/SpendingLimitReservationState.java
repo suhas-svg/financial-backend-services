a9f7cd78287285f68a5a7b9a74ba35ba07c98655
Suhasan Kanagala <57094456+suhas-svg@users.noreply.github.com>
2026-07-27 23:25:28 +0530

    Add spending reservation lifecycle states

---
+++ account-service/src/main/java/com/suhasan/finance/account_service/entity/SpendingLimitReservationState.java
@@ -0,0 +1,9 @@
+package com.suhasan.finance.account_service.entity;
+
+public enum SpendingLimitReservationState {
+    RESERVED,
+    CONSUMED,
+    RELEASED,
+    EXPIRED,
+    RECONCILIATION_REQUIRED
+}
